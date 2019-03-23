ad6912393904cd3e03329374b1e5402155ce9cd6
begatim <begi_1995@hotmail.com>
2019-03-23 11:01:20 +0100

    first commit

---
+++ Diffie-Hellman/Diffie-Hellman/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Diffie_Hellman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtG = new System.Windows.Forms.TextBox();
+            this.txtP = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblG = new System.Windows.Forms.Label();
+            this.lblP = new System.Windows.Forms.Label();
+            this.lblGabimiP = new System.Windows.Forms.Label();
+            this.lblGabimiG = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtG
+            // 
+            this.txtG.Location = new System.Drawing.Point(232, 38);
+            this.txtG.Name = "txtG";
+            this.txtG.Size = new System.Drawing.Size(100, 20);
+            this.txtG.TabIndex = 1;
+            this.txtG.TextChanged += new System.EventHandler(this.txtG_TextChanged);
+            this.txtG.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // txtP
+            // 
+            this.txtP.Location = new System.Drawing.Point(54, 38);
+            this.txtP.Name = "txtP";
+            this.txtP.Size = new System.Drawing.Size(100, 20);
+            this.txtP.TabIndex = 0;
+            this.txtP.TextChanged += new System.EventHandler(this.txtP_TextChanged);
+            this.txtP.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(54, 92);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 2;
+            this.txtA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(232, 92);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 3;
+            this.txtB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblB);
+            this.groupBox1.Controls.Add(this.lblA);
+            this.groupBox1.Controls.Add(this.lblG);
+            this.groupBox1.Controls.Add(this.lblP);
+            this.groupBox1.Controls.Add(this.lblGabimiP);
+            this.groupBox1.Controls.Add(this.lblGabimiG);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtB);
+            this.groupBox1.Controls.Add(this.txtA);
+            this.groupBox1.Controls.Add(this.txtP);
+            this.groupBox1.Controls.Add(this.txtG);
+            this.groupBox1.Location = new System.Drawing.Point(31, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 189);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Vlerat Hyrese";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblB.Location = new System.Drawing.Point(229, 108);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(0, 13);
+            this.lblB.TabIndex = 14;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblA.Location = new System.Drawing.Point(54, 108);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(0, 13);
+            this.lblA.TabIndex = 13;
+            // 
+            // lblG
+            // 
+            this.lblG.AutoSize = true;
+            this.lblG.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblG.Location = new System.Drawing.Point(229, 61);
+            this.lblG.Name = "lblG";
+            this.lblG.Size = new System.Drawing.Size(0, 13);
+            this.lblG.TabIndex = 12;
+            // 
+            // lblP
+            // 
+            this.lblP.AutoSize = true;
+            this.lblP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblP.Location = new System.Drawing.Point(54, 61);
+            this.lblP.Name = "lblP";
+            this.lblP.Size = new System.Drawing.Size(0, 13);
+            this.lblP.TabIndex = 11;
+            // 
+            // lblGabimiP
+            // 
+            this.lblGabimiP.AutoSize = true;
+            this.lblGabimiP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblGabimiP.Location = new System.Drawing.Point(54, 61);
+            this.lblGabimiP.Name = "lblGabimiP";
+            this.lblGabimiP.Size = new System.Drawing.Size(0, 13);
+            this.lblGabimiP.TabIndex = 10;
+            // 
+            // lblGabimiG
+            // 
+            this.lblGabimiG.AutoSize = true;
+            this.lblGabimiG.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblGabimiG.Location = new System.Drawing.Point(229, 61);
+            this.lblGabimiG.Name = "lblGabimiG";
+            this.lblGabimiG.Size = new System.Drawing.Size(0, 13);
+            this.lblGabimiG.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Gjenero";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(206, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "b:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "p :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(206, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "g :";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(31, 242);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(378, 159);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Vlerat Dalese";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 442);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Diffie-Hellman";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtG;
+        private System.Windows.Forms.TextBox txtP;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblGabimiG;
+        private System.Windows.Forms.Label lblGabimiP;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblG;
+        private System.Windows.Forms.Label lblP;
+    }
+}
+
---
+++ Diffie-Hellman/Diffie-Hellman/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Diffie_Hellman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (txtA.Text == "" || txtB.Text == "" || txtG.Text == "" || txtP.Text == "")
+            {
+                lblA.Text = "";
+                lblB.Text = "";
+                lblP.Text = "";
+                lblG.Text = "";
+                if (txtP.Text == "" || !PrimeNumber(BigInteger.Parse(txtP.Text)))
+                {
+                    lblP.Text = "*Plotesojeni kete fushe \n me numer te thjeshte!";
+
+                }
+           
+                if (txtG.Text == "")
+                {
+                    lblG.Text = "*Plotesojeni kete fushe !";
+                }
+             
+                if (txtA.Text == "")
+                {
+                    lblA.Text = "*Plotesojeni kete fushe !";
+                }
+              
+                if (txtB.Text == "")
+                {
+                    lblB.Text = "*Plotesojeni kete fushe !";
+                }
+
+                richTextBox1.Text = "";
+            }
+            else
+            {
+                lblA.Text = "";
+                lblB.Text = "";
+                lblP.Text = "";
+                lblG.Text = "";
+
+                BigInteger g = BigInteger.Parse(txtG.Text);
+                BigInteger p = BigInteger.Parse(txtP.Text);
+                if (!PrimeNumber(p))
+                {
+
+
+                    lblP.Text = "*Nuk eshte numer i thjeshte!";
+
+
+                    richTextBox1.Text = "";
+
+                }
+                else if (!Kontrollo(g, p))
+                {
+                    lblG.Text = "Numri nuk eshte \ngjernerator i p !";
+                    richTextBox1.Text = "";
+                }
+                else
+                {
+                    lblG.Text = "";
+                    BigInteger a = BigInteger.Parse(txtA.Text);
+                    BigInteger b = BigInteger.Parse(txtB.Text);
+                  
+                 
+
+                        BigInteger VleraA = BigInteger.ModPow(g, a, p);
+
+                        BigInteger VleraB = BigInteger.ModPow(g, b, p);
+
+                        BigInteger CelesiA = BigInteger.ModPow(VleraB, a, p);
+                        BigInteger CelesiB = BigInteger.ModPow(VleraA, b, p);
+                        string mesazhi = "Vlera e a =" + a + "\nVlera e b =" + b + "\nVlera e Celesi i Gjereneraur i A=" + CelesiA + "\nVlera e Celesi i Gjereneraur i B=" + CelesiB;
+
+                        richTextBox1.Text = mesazhi;
+
+
+                        lblGabimiP.Text = "";
+                    
+                }
+            }
+        }
+        public bool Kontrollo(BigInteger num, BigInteger p)
+        {
+            if (num >= p)
+            {
+                return false;
+            }
+            else
+            {
+                BigInteger[] vargu = new BigInteger[(int)p - 1];
+                BigInteger[] vargu2 = new BigInteger[(int)p - 1];
+
+                for (int i = 1; i <= (p - 1); i++)
+                {
+                    vargu2[i - 1] = i;
+
+                }
+                for (int i = 1; i <= (p - 1); i++)
+                {
+                    vargu[i - 1] = BigInteger.ModPow(num, i, p);
+
+                }
+                Array.Sort(vargu);
+
+                return vargu.SequenceEqual(vargu2);
+            }
+        }
+        public bool PrimeNumber(BigInteger number)
+        {
+            if (number > 1)
+            {
+                for (int i = 2; i <= number - 1; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+                    }
+
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+     
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsLetter(e.KeyChar) || Char.IsWhiteSpace(e.KeyChar) )
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsLetter(e.KeyChar) || Char.IsWhiteSpace(e.KeyChar) )
+            {
+                e.Handled = true;
+            }
+
+        }
+
+       
+
+        }
+    }
+}
